9fdc1640f60d873b3f6255209343c18d8625fa6e
Marek Buszman <mbusiq@gmail.com>
2014-03-20 09:57:44 +0100

    another specs

--- spec/salesforce_endpoint_spec.rb
+++ spec/salesforce_endpoint_spec.rb
@@ -3,20 +3,23 @@ require 'spec_helper'
 describe SalesforceEndpoint do
   include_examples 'config hash'
 
-  context 'order webhooks' do
-    describe '/add_order' do
-      let(:payload) do
-        payload = Factories.add_order_payload
-        payload.merge(parameters: config)
-      end
+  context 'webhooks' do
+    ['add_order', 'update_order', 'add_customer', 'update_customer'].each do |path|
+      describe path do
+        let(:payload) do
+          payload = Factories.send("#{path}_payload")
+          payload.merge(parameters: config)
+        end
+        let(:customer_email){ payload["order"].nil? ? payload["customer"]["email"] : payload["order"]["email"] }
 
-      it 'works' do
-        VCR.use_cassette 'requests/add_order' do
-          post '/add_order', payload.to_json, auth
-          expect(last_response).to be_ok
-          body = JSON.parse(last_response.body)
-          expect(body["request_id"]).to eq payload["request_id"]
-          expect(body["summary"]).to eq "Successfully upserted contact for #{payload["order"]["email"]}"
+        it 'works' do
+          VCR.use_cassette "requests/#{path}" do
+            post "/#{path}", payload.to_json, auth
+            expect(last_response).to be_ok
+            body = JSON.parse(last_response.body)
+            expect(body["request_id"]).to eq payload["request_id"]
+            expect(body["summary"]).to eq "Successfully upserted contact for #{customer_email}"
+          end
         end
       end
     end

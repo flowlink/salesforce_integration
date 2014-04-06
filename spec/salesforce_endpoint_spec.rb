ae486859e93e82b452a21de6712553cd427440b6
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-04-06 14:02:09 +0100

    add test for upserting products

--- spec/salesforce_endpoint_spec.rb
+++ spec/salesforce_endpoint_spec.rb
@@ -4,6 +4,26 @@ describe SalesforceEndpoint do
   include_examples 'config hash'
 
   context 'webhooks' do
+    ['add_order', 'update_order', 'add_customer', 'update_customer', 'add_product'].each do |path|
+      describe path do
+        let(:payload) do
+          payload = Factories.send("#{path}_payload")
+          payload.merge(parameters: config)
+        end
+
+        it 'works' do
+          VCR.use_cassette "requests/#{path}" do
+            post "/#{path}", payload.to_json, auth
+            expect(last_response).to be_ok
+            body = JSON.parse(last_response.body)
+            expect(body["request_id"]).to eq payload["request_id"]
+          end
+        end
+      end
+    end
+  end
+
+  describe "upserting customers" do
     ['add_order', 'update_order', 'add_customer', 'update_customer'].each do |path|
       describe path do
         let(:payload) do
@@ -15,10 +35,28 @@ describe SalesforceEndpoint do
         it 'works' do
           VCR.use_cassette "requests/#{path}" do
             post "/#{path}", payload.to_json, auth
-            expect(last_response).to be_ok
             body = JSON.parse(last_response.body)
-            expect(body["request_id"]).to eq payload["request_id"]
             expect(body["summary"]).to eq "Successfully upserted contact for #{customer_email}"
+          end
+        end
+      end
+    end
+  end
+
+  describe "upserting products" do
+    ['add_product'].each do |path|
+      describe path do
+        let(:payload) do
+          payload = Factories.send("#{path}_payload")
+          payload.merge(parameters: config)
+        end
+        let(:product_code){ payload["product"]["sku"] }
+
+        it 'works' do
+          VCR.use_cassette "requests/#{path}" do
+            post "/#{path}", payload.to_json, auth
+            body = JSON.parse(last_response.body)
+            expect(body["summary"]).to eq "Successfully upserted product for #{product_code}"
           end
         end
       end

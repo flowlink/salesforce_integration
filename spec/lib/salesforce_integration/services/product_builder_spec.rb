1f94fd39a1db7fc8b6cf2b078ef237a8792c31eb
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-04-07 13:53:26 +0100

    add spec for product builder

--- spec/lib/salesforce_integration/services/product_builder_spec.rb
+++ spec/lib/salesforce_integration/services/product_builder_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe SalesforceIntegration::Service::ContactBuilder do
-  let(:payload) { Factories.add_order_payload }
-  subject { described_class.new(payload['order']) }
+describe SalesforceIntegration::Service::ProductBuilder do
+  let(:payload) { Factories.add_product_payload }
+  subject { described_class.new(payload['product']) }
 
   context 'methods' do
     describe '#build' do
@@ -10,7 +10,7 @@ describe SalesforceIntegration::Service::ContactBuilder do
 
       it 'returns hash with address data' do
         expect(method).to be_kind_of Hash
-        expect(method.keys.count).to eq 15
+        expect(method.keys.count).to eq 4
       end
     end
   end

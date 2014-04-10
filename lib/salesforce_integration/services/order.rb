218e3471c6d5a99570ccabcf93d09065112709e9
blazejhadzik <blazej.hadzik@netguru.pl>
2014-04-10 22:36:33 +0200

    order service update to assign customer with order
    [#68800266]

--- lib/salesforce_integration/services/order.rb
+++ lib/salesforce_integration/services/order.rb
@@ -6,14 +6,19 @@ module SalesforceIntegration
         super("Order__c", config)
       end
 
-      def find_id_by_email(email)
-        results = salesforce.query("select Id from Order__c where Email__c = '#{email}'")
-        results.any? ? results.first['Id'] : nil
+      def is_present?(id)
+        salesforce.query("select Id from Order__c where Id__c = '#{id}'").any?
       end
 
-      def upsert!(email, attributes = {})
-        contact_id = find_id_by_email(email)
-        contact_id.present? ? update!(attributes.merge({ Id: contact_id })) : create!(attributes)
+      def find_client(email)
+        results = salesforce.query("select Id from Contact where Email = '#{email}'")
+        results.any? ? results.first.fetch('Id') : nil
+      end
+
+      def upsert!(order_attr = {})
+        contact_id = find_client(order_attr['Email__c'])
+        order_attr = order_attr.merge({ 'ContactId__c'=> contact_id }) if contact_id.present?
+        is_present?(order_attr.fetch 'Id__c') ? update!(order_attr) : create!(order_attr)
       end
 
     end

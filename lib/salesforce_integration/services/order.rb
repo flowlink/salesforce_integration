3989db399600a8b9cb1fb36e9721cc7d0cf0e05d
blazejhadzik <blazej.hadzik@netguru.pl>
2014-04-10 10:19:03 +0200

    order class - to be extended
    [#68800266]

---
+++ lib/salesforce_integration/services/order.rb
@@ -0,0 +1,22 @@
+module SalesforceIntegration
+  module Service
+    class Order < Base
+
+      def initialize(config)
+        super("Order__c", config)
+      end
+
+      def find_id_by_email(email)
+        results = salesforce.query("select Id from Order__c where Email__c = '#{email}'")
+        results.any? ? results.first['Id'] : nil
+      end
+
+      def upsert!(email, attributes = {})
+        contact_id = find_id_by_email(email)
+        binding.pry
+        contact_id.present? ? update!(attributes.merge({ Id: contact_id })) : create!(attributes)
+      end
+
+    end
+  end
+end

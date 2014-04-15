eb914b4767fa9b22d804dbdee6e24ced92f8090b
blazejhadzik <blazej.hadzik@netguru.pl>
2014-04-15 22:48:06 +0200

    cleanup
    [#69350168]

--- lib/integrations/order.rb
+++ lib/integrations/order.rb
@@ -9,15 +9,6 @@ module Integration
     end
 
     def upsert!
-      create_contact
-      create_order
-    end
-
-    def create_contact
-      Integration::ContactAccount.new(config, contact_attributes).upsert!
-    end
-
-    def create_order
       order_service.upsert!(order_params)
     end
 
@@ -25,42 +16,6 @@ module Integration
 
     def order_params
       Integration::Builder::Order.new(object['order']).build
-    end
-
-    def contact_attributes
-      {
-        'firstname'        => first_name,
-        'lastname'         => last_name,
-        'email'            => email,
-        'shipping_address' => shipping_address,
-        'billing_address'  => billing_address,
-      }
-    end
-
-    def first_name
-      shipping_address['firstname'] || billing_address['firstname']
-    end
-
-    def last_name
-      shipping_address['lastname'] || billing_address['lastname']
-    end
-
-    def email
-      lookup_item('email')
-    end
-
-    def billing_address
-      lookup_item('billing_address')
-    end
-
-    def shipping_address
-      lookup_item('shipping_address')
-    end
-
-    private
-
-    def lookup_item(item)
-      object['order'].fetch item
     end
 
   end

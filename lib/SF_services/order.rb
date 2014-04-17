6ea907166507213fcc75fb001b574d99d647d660
blazejhadzik <blazej.hadzik@netguru.pl>
2014-04-17 16:57:31 +0200

    merge order hash to assign account
    [#69685770]

--- lib/SF_services/order.rb
+++ lib/SF_services/order.rb
@@ -14,9 +14,18 @@ module SFService
       results.any? ? results.first.fetch('Id') : nil
     end
 
+    def find_account_id_by_email(email)
+      results = salesforce.query("select Account__c from Contact where Email = '#{email}'")
+      results.any? ? results.first['Account__c'] : nil
+    end
+
     def upsert!(order_attr = {})
-      contact_id = find_client(order_attr.fetch 'Email__c')
-      order_attr = order_attr.merge({ 'ContactId__c'=> contact_id }) if contact_id.present?
+      email = order_attr.fetch 'Email__c'
+      contact_id  = find_client(email)
+      account_id  = find_account_id_by_email(email)
+
+      order_attr = order_attr.merge( { 'ContactId__c' => contact_id } ) if contact_id.present?
+      order_attr = order_attr.merge( { 'Account__c' => account_id } ) if account_id.present?
 
       order_id = is_present?(order_attr.fetch 'Id__c')
       order_id.present? ? update!(order_attr.merge({ Id: order_id })) : create!(order_attr)

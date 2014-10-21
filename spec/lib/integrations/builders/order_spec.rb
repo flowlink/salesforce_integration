36379574d3bb278904a74ef31cd8c317787d2a93
Pablo Cantero <pablo@pablocantero.com>
2014-10-21 16:53:36 -0200

    IN-392 Fix order spec

--- spec/lib/integrations/builders/order_spec.rb
+++ spec/lib/integrations/builders/order_spec.rb
@@ -10,7 +10,7 @@ describe Integration::Builder::Order do
     it 'returns hash with address data' do
       expect(result).to be_kind_of Hash
       expect(result.keys).to eq(
-        ["Id__c", "Status__c", "Channel__c", "Email__c", "Currency__c", "PlacedOn__c", "Subtotal__c", "Adjustment__c", "Tax__c", "Shipping__c", "Payment__c", "Total__c"]
+        ["Name", "Status__c", "Channel__c", "Email__c", "Currency__c", "PlacedOn__c", "Subtotal__c", "Adjustment__c", "Tax__c", "Shipping__c", "Payment__c", "Total__c"]
       )
     end
   end

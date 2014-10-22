63e928986f22e5d0a58398bb9937efa80fe44964
Reinaldo Braga <reinaldob@gmail.com>
2014-10-22 11:04:37 -0200

    IN-392 Adds payments to oportunity notes

--- lib/SF_services/payment.rb
+++ lib/SF_services/payment.rb
@@ -1,32 +1,25 @@
 module SFService
   class Payment < Base
     def initialize(config)
-      super("Payment__c", config)
+      super("Note", config)
     end
 
     def is_present?(number)
-      results = salesforce.query("select Id from Payment__c where Name = '#{number}'")
+      results = salesforce.query("select Id from Note where Title = 'Payment:#{number}'")
       results.any? ? results.first.fetch('Id') : nil
     end
 
     def find_order(id)
-      results = salesforce.query("select Id from Order__c where Name = '#{id}'")
+      results = salesforce.query("select Id from Opportunity where Name = '#{id}'")
       results.any? ? results.first['Id'] : nil
     end
 
-    def find_account_id_by_email(email)
-      results = salesforce.query("select Account__c from Contact where Email = '#{email}'")
-      results.any? ? results.first['Account__c'] : nil
-    end
-
     def upsert!(payment_attr = {}, order_code, email)
-      order_id    = find_order(order_code)
-      account_id  = find_account_id_by_email(email)
+      order_id = find_order(order_code)
 
-      payment_attr = payment_attr.merge( { 'Order__c' => order_id } ) if order_id.present?
-      payment_attr = payment_attr.merge( { 'Account__c' => account_id } ) if account_id.present?
+      payment_attr = payment_attr.merge( { 'ParentId' => order_id } ) if order_id.present?
 
-      payment_id = is_present?(payment_attr.fetch 'Name')
+      payment_id = is_present?(payment_attr.fetch 'Title')
       payment_id.present? ? update!(payment_attr.merge({ Id: payment_id })) : create!(payment_attr)
     end
   end

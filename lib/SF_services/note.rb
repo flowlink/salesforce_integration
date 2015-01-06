7d6df1e0ed237ef5e963ec16f52b360b5cc6a271
Washington Luiz <huoxito@gmail.com>
2015-01-05 21:35:01 -0300

    Fix Note (returns, payments, shipments) update

--- lib/SF_services/note.rb
+++ lib/SF_services/note.rb
@@ -11,10 +11,13 @@ module SFService
     end
 
     def upsert!(attributes = {}, opportunity_id)
-      attributes = attributes.merge ParentId: opportunity_id
-
       note_id = find_note_by_title attributes['Title'], opportunity_id
-      note_id.present? ? update!(attributes.merge(Id: note_id)) : create!(attributes)
+
+      if note_id.present?
+        update! attributes.merge(Id: note_id)
+      else
+        create! attributes.merge(ParentId: opportunity_id)
+      end
     end
   end
 end

a2317d5a57b26285f8531f160d18bd3150de0ea7
Washington Luiz <huoxito@gmail.com>
2015-01-23 15:34:08 -0300

    drop pending specs

--- spec/lib/SF_services/product_spec.rb
+++ spec/lib/SF_services/product_spec.rb
@@ -8,23 +8,4 @@ describe SFService::Product do
     subject { super().model_name }
     it { should eq 'Product2' }
   end
-
-  describe '#upsert!' do
-    pending 'creates product if it doesnt exist' do
-      allow(subject).to receive(:find_id_by_code).and_return(nil)
-      allow(subject.send(:salesforce)).to receive(:create!).and_return(true)
-
-      expect(subject.send(:salesforce)).to receive(:create!).once
-
-      subject.upsert!('T-Shirt', {ProductCode: 'T-Shirt'})
-    end
-
-    pending 'updates product if it exists' do
-      allow(subject).to receive(:find_id_by_code).and_return('123')
-      allow(subject.send(:salesforce)).to receive(:update!).and_return(true)
-
-      expect(subject.send(:salesforce)).to receive(:update!).once
-      subject.upsert!('T-Shirt', {ProductCode: 'T-Shirt'})
-    end
-  end
 end
